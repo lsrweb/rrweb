fd6aaa4e616fdfaf7c60931c03b65d85244f6183
KuCaiTx <siriforever.ltd@gmail.com>
2024-06-13 12:54:10 +0800

    feat: Clear rrweb data before storing new events

--- rrwebt_backend/my-rrweb-service/src/controllers/storeController.js
+++ rrwebt_backend/my-rrweb-service/src/controllers/storeController.js
@@ -12,6 +12,15 @@ class StoreController {
       console.log("key:", key); // 存储的 key
       console.log("event:", event); // 存储的 event,前端每次传递一个事件
 
+      // 每次存储之前,都清空之前的数据
+      fs.writeFile(`public/stores/${key}.txt`, "", (err) => {
+        if (err) {
+          console.error("无法清空rrweb数据:", err);
+          reject(err);
+        } else {
+          console.log("成功清空rrweb数据");
+        }
+      }
       // 向根目录public/stores/文件中写入数据
       // 判断是否有 key.txt 文件，如果有则追加，如果没有则创建
       // 每行存储一个事件
