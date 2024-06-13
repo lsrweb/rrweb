f70de316de26651d6115b76faab5c915243eeb6b
KuCaiTx <siriforever.ltd@gmail.com>
2024-06-13 14:37:36 +0800

    l

--- rrwebt/src/App.vue
+++ rrwebt/src/App.vue
@@ -1,56 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <input type="text" v-model="msg">
-  <!-- 开始录制  -->
-  <button @click="start">开始录制</button>
-  <!-- 结束录制  -->
-  <button @click="stop">结束录制</button>
-  <!-- 播放录制  -->
-  <button @click="play">播放录制</button>
-
-  <!-- file input -->
-  <input type="file" @change="fileChange" />
-  <img :src="imgUrl" alt="">
-
-
-  <div class="replay"></div>
+  <div id="app">
+    <index msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import { startRecording,stopRecording,playRecording } from '@/test';
-import HelloWorld from './components/HelloWorld.vue'
-import rrwebPlayer from 'rrweb-player';
-import 'rrweb-player/dist/style.css';
+import index from './views/index.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      msg: 'Hello World',
-      imgUrl: ''
-    }
-  },
-  methods: {
-    start() {
-      startRecording();
-    },
-    stop() {
-      stopRecording();
-    },
-    play() {
-      playRecording(rrwebPlayer, document.querySelector('.replay'));
-    },
-    fileChange(e) {
-      // 获取文件
-      const file = e.target.files[0];
-      // file 转为 url
-      this.imgUrl = URL.createObjectURL(file);
-
-    }
+    index
   }
 }
 </script>
--- rrwebt/src/main.js
+++ rrwebt/src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/styles/index.css';
 
-createApp(App).mount('#app')
+Vue.use(ElementUI);
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
