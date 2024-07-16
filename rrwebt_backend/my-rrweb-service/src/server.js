195bc08361f4d5eafd0f3e6a1108c4a08cc2cc32
KuCaiTx <siriforever.ltd@gmail.com>
2024-07-16 15:08:25 +0800

    feat: Add video streaming endpoint for rrwebt project

--- rrwebt_backend/my-rrweb-service/src/server.js
+++ rrwebt_backend/my-rrweb-service/src/server.js
@@ -3,6 +3,10 @@ const app = express();
 const storeRouter = require('./routes/store');
 const retrieveRouter = require('./routes/retrieve');
 
+const fs = require('fs');
+const path = require('path');
+
+
 // 跨域
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -10,8 +14,52 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+
+
+
+
+app.get('/video', (req, res) => {
+  const videoPath = path.join(__dirname, '../public/video/希望.mp4');
+  const stat = fs.statSync(videoPath);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1;
+
+    if(start >= fileSize) {
+      res.status(416).send('Requested range not satisfiable\n'+start+' >= '+fileSize);
+      return;
+    }
+    
+    const chunksize = (end-start)+1;
+    const file = fs.createReadStream(videoPath, {start, end});
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(videoPath).pipe(res);
+  }
+});
+
+
 // 设置静态资源目录
 app.use(express.static('public'));
+
+
 // Middleware
 app.use(express.json({
   limit: '50mb'
